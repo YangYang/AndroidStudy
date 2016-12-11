20945a5e4dd28c165c25936c13aa4f63bdda8024
IMUDGES-YY <929144090@qq.com>
2016-12-11 17:30:00 +0800

    PopupWindow 简单函数学习测试

--- src/com/imudges/AndroidStudy/PopupWindowTestActity.java
+++ src/com/imudges/AndroidStudy/PopupWindowTestActity.java
@@ -27,7 +27,6 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
     Button btn2;
     Button btn3;
     Button btn4;
-    Button btn5;
 
 
     @Override
@@ -43,7 +42,6 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
         btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
         btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
     }
 
     private void initData(){
@@ -51,7 +49,6 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
         btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
         btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
         /**
          * .inflate()，查找layout/res 下的一个xml布局
          * */
@@ -64,18 +61,8 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
             case R.id.btn1:{
                 Log.v("1","点击了btn1");
                 if(mPopupWindow !=null){
-                   /* mPopupWindow.setTouchInterceptor(new View.OnTouchListener() {
-                        @Override
-                        public boolean onTouch(View view, MotionEvent motionEvent) {
-                            if(motionEvent.getAction() == MotionEvent.ACTION_OUTSIDE){
-                                mPopupWindow.dismiss();
-                                return true;
-                            }
-                            return false;
-                        }
-                    });*/
                     Log.v("!","进入btn1的if");
-                    mPopupWindow.setTouchable(false);
+                    mPopupWindow.setTouchable(false);//控件可被点击
                     mPopupWindow.setOutsideTouchable(true);
                     mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
                     mPopupWindow.showAsDropDown(view);
@@ -83,18 +70,8 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
                 else{
                     Log.v("!","进入btn1的else");
                     mPopupWindow = new PopupWindow(getLayoutInflater().inflate(R.layout.popupwindow_test,null), MATCH_PARENT,MATCH_PARENT,true);
-                    /**
-                     * setTouchable()参数
-                     * true：点击加载出来的view 不消失，点击加载它的控件 view 消失（相当于点击空白区域）
-                     * false: 点击加载出来的view 消失，点击加载它的控件 会再次弹出
-                     * */
                     mPopupWindow.setTouchable(true);
-                    /**
-                     * setOutsideTouchable()
-                     * true:点击空白区域，加载出的控件会消失，点返回也会消失
-                     * false：点击空白区域不会消失，点击返回会消失
-                     * */
-                    mPopupWindow.setOutsideTouchable(false);
+                    mPopupWindow.setOutsideTouchable(true);
                     mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
                     mPopupWindow.showAsDropDown(view);
                 }
@@ -104,6 +81,15 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
                 Log.v("2","点击了btn2");
                 if(mPopupWindow !=null) {
                     Log.v("!","进入btn2的if");
+                    mPopupWindow.setTouchable(false);//控件不可被点击
+                    mPopupWindow.setOutsideTouchable(false);
+                    mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+                    mPopupWindow.update();
+                    mPopupWindow.showAsDropDown(view);
+                }
+                else{
+                    Log.v("!","进入btn2的else");
+                    mPopupWindow = new PopupWindow(getLayoutInflater().inflate(R.layout.popupwindow_test,null), MATCH_PARENT,MATCH_PARENT,true);
                     mPopupWindow.setTouchable(false);
                     mPopupWindow.setOutsideTouchable(false);
                     mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
@@ -112,14 +98,50 @@ public class PopupWindowTestActity extends Activity implements Button.OnClickLis
                 break;
             }
             case R.id.btn3:{
-
+                Log.v("3","点击了btn3");
+                if(mPopupWindow !=null) {
+                    Log.v("!","进入btn3的if");
+                    mPopupWindow.setTouchable(false);
+                    mPopupWindow.setOutsideTouchable(true);
+                    mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+                    mPopupWindow.showAsDropDown(view);
+                }
+                else{
+                    Log.v("!","进入btn3的else");
+                    mPopupWindow = new PopupWindow(getLayoutInflater().inflate(R.layout.popupwindow_test,null), MATCH_PARENT,MATCH_PARENT,true);
+                    mPopupWindow.setTouchable(false);
+                    mPopupWindow.setOutsideTouchable(true);
+                    mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+                    mPopupWindow.showAsDropDown(view);
+                }
+                break;
             }
             case R.id.btn4:{
-
-            }
-            case R.id.btn5:{
-
+                Log.v("4","点击了btn4");
+                if(mPopupWindow !=null) {
+                    Log.v("!","进入btn4的if");
+                    mPopupWindow.setFocusable(false);
+                    /**
+                     * 点击返回会直接结束Activity，程序报错，对于setTouchable和setOutsideTouchable，与之前总结的结果相同
+                     * */
+                    mPopupWindow.setTouchable(false);
+                    mPopupWindow.setOutsideTouchable(false);
+                    mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+                    mPopupWindow.showAsDropDown(view);
+                }
+                else{
+                    Log.v("!","进入btn4的else");
+                    mPopupWindow = new PopupWindow(getLayoutInflater().inflate(R.layout.popupwindow_test,null), MATCH_PARENT,MATCH_PARENT,false);
+                    mPopupWindow.setTouchable(false);
+                    mPopupWindow.setOutsideTouchable(true);
+                    mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+                    mPopupWindow.showAsDropDown(view);
+                }
+                break;
             }
         }
+    }
+    public void _onClick(View view){
+        Log.v("PopupWindow","Button被点击");
     }
 }
